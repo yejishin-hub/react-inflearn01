47ac6ac27af432a3eab6ef6615a857c3e0cafd93
Yeji Shin <67362015+yejishin-hub@users.noreply.github.com>
2026-02-02 12:44:34 +0000

    2026-02-02 React Ch3-1. Component

---
+++ src/3-1/App.js
@@ -0,0 +1,13 @@
+// import Hello from "./Hello.js";
+// import World from "./World.js";
+import Hello from "./Hello";
+import World from "./World";
+
+export default function App() {
+  return (
+    <div>
+      <Hello />
+      <World />
+    </div>
+  );
+}
